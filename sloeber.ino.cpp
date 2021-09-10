85cf302a9ccfef8b7946b83c292605e6772e3044
Marcelo Valdez <valdez@fadeasa.com.ar>
2021-09-10 14:59:00 -0300

    minor changes

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2021-09-09 13:53:39
+//This file has been generated on 2021-09-10 10:19:30
 
 #include "Arduino.h"
 #define fadea
